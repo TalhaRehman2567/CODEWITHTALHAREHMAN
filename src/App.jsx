a8e2c30ac6f95b1bd8e50ba69f12afae07903376
TalhaRehman2567 <talharehman2567@gmail.com>
2026-02-14 11:25:22 +0500

    Implement Navbar Components

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,21 @@ import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsC
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
+    <div className="relative  z-0 bg-primary">
           <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
               <Navbar />
-              <Hero />
+              {/* <Hero /> */}
           </div>
           <div className="">
-            <About />
+            {/* <About />
             <Experience />
             <Tech />
             <Works />
-            <Feedbacks />
+            <Feedbacks /> */}
           </div>
           <div className="relative z-0">
-             <Contact />
-             <StarsCanvas />
+             {/* <Contact />
+             <StarsCanvas /> */}
           </div>
     </div>
     </BrowserRouter>
